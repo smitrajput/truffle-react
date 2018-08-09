92fd46d99638961e46dd249906c0c1d559db0771
Smit Rajput <smitrajputtd@gmail.com>
2018-08-09 13:48:10 +0530

    Modify UI for video

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
 
     this.state = {
       storageValue: 0,
-      web3: null
+      web3: null,
     }
   }
 
@@ -77,7 +77,7 @@ class App extends Component {
         <main className="container">
           <div className="pure-g">
             <div className="pure-u-1-1">
-              <h1>Good to Go!</h1>
+              <h1>Here you go!</h1>
               <p>Your Truffle Box is installed and ready.</p>
               <h2>Smart Contract Example</h2>
               <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
